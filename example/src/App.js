2c646ba6e0c0f211f53f0f4cbe0a333598476be7
Marek Dano <mk.dano@gmail.com>
2019-11-07 08:18:23 +0000

    Define useQuote hook function and add example of it.

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { useMyHook } from '@marekdano/use-quote'
+import { useQuote } from '@marekdano/use-quote'
 
 const App = () => {
-  const example = useMyHook()
-  return (
-    <div>
-      {example}
-    </div>
-  )
+  const { quote, isLoading, isError } = useQuote()
+  
+  if (isLoading) return <p>Loading...</p>
+
+  if (quote) {
+    return (
+      <>
+        <p>{quote.text}</p>
+        <p><i>{quote.author}</i></p>
+      </>
+    )
+  }
+
+  if (isError) return <p>Difficulty displaying a quote at the moment.</p>
+
+  return null
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import * as React from 'react'
+import { useState, useEffect } from 'react'
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
+export const useQuote = () => {
+  const url = `https://quote-garden.herokuapp.com/quotes/random`
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
+  const [quote, setQuote] = useState({text: '', author: ''})
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    let mounted = true;
+    async function getQuote() {
+      setIsError(false)
+      setIsLoading(true)
+
+      try {
+        // JSON object from web API has structure of { "_id": string, "quoteText": string, "quoteAuthor": string }
+        const response = await fetch(url)
+        const { quoteText: text, quoteAuthor } = await response.json()
+        const author = quoteAuthor || 'Anonymous'
+
+        if (mounted) setQuote({text, author})
+      } catch (error) {
+        if (mounted) setIsError(true)
+      }
+
+      if (mounted) setIsLoading(false)
+    };
+
+    getQuote();
+
     return () => {
-      window.clearInterval(interval)
+      mounted = false
     }
   }, [])
 
-  return counter
+  return { quote, isLoading, isError }
 }
